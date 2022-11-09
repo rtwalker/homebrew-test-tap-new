19600ddbf11e7635c476523361c673c7e16cb32d
Ryan Walker <rwalker@timescale.com>
2022-11-09 14:46:21 -0500

    timescaledb-toolkit 1.12.0 (new formula)

---
+++ Formula/timescaledb-toolkit.rb
@@ -0,0 +1,59 @@
+class TimescaledbToolkit < Formula
+  desc "Extension with hyperfunctions, fully compatible with TimescaleDB and PostgreSQL"
+  homepage "https://www.timescale.com"
+  url "https://github.com/rtwalker/timescaledb-toolkit/archive/refs/tags/1.12.0.tar.gz"
+  sha256 "a4fe2079880d6ab7b26f9a38510fd099beae81288e40e928622738bc0d9ded58"
+  head "https://github.com/timescale/timescaledb-toolkit.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "rustfmt" => :build
+  depends_on "postgresql@14"
+
+  def postgresql
+    Formula["postgresql@14"]
+  end
+
+  resource "cargo-pgx" do
+    url "https://github.com/tcdi/pgx/archive/refs/tags/v0.5.4.tar.gz"
+    sha256 "7c6b5e7fdc6b974b726bb4b965ae8e9dacecc059db1a59b1ec471edb090b4454"
+  end
+
+  def install
+    ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
+    ENV["PGX_HOME"] = buildpath
+
+    resource("cargo-pgx").stage "pgx"
+    system "cargo", "install", "--locked", "--path", "pgx/cargo-pgx"
+    system "cargo", "pgx", "init", "--pg14", "pg_config"
+
+    cd "extension" do
+      system "cargo", "pgx", "package"
+    end
+
+    system "cargo", "run", "--manifest-path", "tools/post-install/Cargo.toml", "--", "--dir",
+"target/release/timescaledb_toolkit-pg14"
+
+    (lib/postgresql.name).install Dir["target/release/**/lib/**/timescaledb_toolkit*.so"]
+    (share/postgresql.name/"extension").install Dir["target/release/**/share/**/timescaledb_toolkit--*.sql"]
+    (share/postgresql.name/"extension").install Dir["target/release/**/share/**/timescaledb_toolkit.control"]
+  end
+
+  test do
+    pg_ctl = postgresql.opt_bin/"pg_ctl"
+    psql = postgresql.opt_bin/"psql"
+    port = free_port
+
+    system pg_ctl, "initdb", "-D", testpath/"test"
+    (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
+
+      shared_preload_libraries = 'timescaledb_toolkit-#{version}'
+      port = #{port}
+    EOS
+    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
+    begin
+      system psql, "-p", port.to_s, "-c", "CREATE EXTENSION \"timescaledb_toolkit\";", "postgres"
+    ensure
+      system pg_ctl, "stop", "-D", testpath/"test"
+    end
+  end
+end
